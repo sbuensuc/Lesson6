b2b400bfd8d74d646a02e6db2d958c19612da72d
Sam <Sam@DESKTOP-O0S9A91>
2016-10-19 09:00:40 -0400

    added logout and register logic

--- Lesson6/Logout.aspx.cs
+++ Lesson6/Logout.aspx.cs
@@ -5,13 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace Lesson6
 {
     public partial class Logout : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // store session info and authentication methods in the authenticationmanager object
+            var autheticationManager = HttpContext.Current.GetOwinContext().Authentication;
 
+            //perform sign out
+            autheticationManager.SignOut();
+            Response.Redirect("~/Login.aspx");
         }
     }
 }
--- Lesson6/Register.aspx.cs
+++ Lesson6/Register.aspx.cs
@@ -5,6 +5,12 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//requiered for Idenitiy and Owin security
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace Lesson6
 {
     public partial class Register : System.Web.UI.Page
@@ -16,12 +22,45 @@ namespace Lesson6
 
         protected void CancelButton_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("~/Default.aspx");
         }
 
         protected void RegisterButton_Click(object sender, EventArgs e)
         {
+            // create a new userstore and usermanager objects
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
 
+            //search for and create a new user object
+
+            var user = new IdentityUser()
+            {
+                UserName = UserNameTextBox.Text,
+                PhoneNumber = PhoneNumberTextBox.Text,
+                Email = EmailTextBox.Text
+            };
+            //create a new user in the db and store results
+
+            IdentityResult result = userManager.Create(user, PasswordTextBox.Text);
+
+            //check if successfully registered
+            if(result.Succeeded)
+            {
+                //authenticate and login our new user
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                // sign in the user
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+                Response.Redirect("~/Contoso/MainMenu.aspx");
+            }
+            else
+            {
+                //display error in alertflash div
+                StatusLabel.Text = result.Errors.FirstOrDefault();
+                AlertFlash.Visible = true;
+            }
         }
     }
 }
